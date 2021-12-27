2bb37492a8563c09f2ae1b50f500425a9cd0158e
Mstars_Lab1_12 <mstars_lab1_12@Mstars-Lab1-12s-iMac.local>
2021-12-27 12:51:55 +0800

    #5 added footer tal

--- project/src/Footer.js
+++ project/src/Footer.js
@@ -1,3 +1,6 @@
+import React from "react";
+
+import './App.css';
 
 const Footer = () => {
 
@@ -5,12 +8,43 @@ const Footer = () => {
 
 
     return (
-       <footer>
-           <div>
-               <img src="../public/icons/food_delivery.png" alt="" />
-               Food delivery
-           </div>
-       </footer> 
+        <footer>
+            <div className="container">
+
+       
+            <div>
+                <img src="./icons/foodDelivery.png" alt="" />
+                Food delivery
+            </div >
+            <div className="row">
+                <div className="col-6">
+                    <p>Нүүр</p>
+                    <p>Хоолны цэс</p>
+                    <p>хүргэлтийн бүс</p>
+                </div>
+                <div className="col-6">
+                    <h2>Холбоо барих</h2>
+                    <p>(976) 77123456</p>
+                    <p>(976) 77123456</p>
+                </div>
+
+            </div>
+            <div className="Logos">
+            <i class="fab fa-facebook"></i>
+            
+
+            </div>
+            <img src="" alt="" srcset="" />
+            <div>
+                <p>Нууцлалын бодлого</p>
+                <p>Үйлчилгээний нөхцөл</p>
+
+            </div>
+            <div className="text-end">
+                <p>2020 Mstars Foods LLC</p>
+            </div>
+            </div>
+        </footer>
     )
 
 }
