d78acc0c92ee95348bc2c462f59e41434983fe27
Mstars_Lab1_12 <mstars_lab1_12@Mstars-Lab1-12s-iMac.local>
2021-12-27 11:24:08 +0800

    #5 icons added

--- project/src/Footer.js
+++ project/src/Footer.js
@@ -1,6 +1,18 @@
 
 const Footer = () => {
 
+
+
+
+    return (
+       <footer>
+           <div>
+               <img src="../public/icons/food_delivery.png" alt="" />
+               Food delivery
+           </div>
+       </footer> 
+    )
+
 }
 
 export default Footer;
