3511237cf1062ea52bbdc116a681d8c8a9385eba
Mstars_Lab1_12 <mstars_lab1_12@Mstars-Lab1-12s-iMac.local>
2021-12-28 11:20:25 +0800

    #5 footer finish

--- project/src/Footer.js
+++ project/src/Footer.js
@@ -1,48 +1,45 @@
 import React from "react";
-
+import foodDelivery from "./icons/foodDelivery.png"
+import facebookicon from './icons/facebook_logo.png'
+import instagramIcon from './icons/instagram_logo.png'
+import twitterIcon from './icons/twitter_logo.png'
+import lineIcon from './icons/Line_19.png'
 import './App.css';
 
 const Footer = () => {
-
-
-
-
     return (
         <footer>
             <div className="container">
-
-       
-            <div>
-                <img src="./icons/foodDelivery.png" alt="" />
-                Food delivery
-            </div >
-            <div className="row">
-                <div className="col-6">
-                    <p>Нүүр</p>
-                    <p>Хоолны цэс</p>
-                    <p>хүргэлтийн бүс</p>
+                <div className="pt-4 ">
+                    <img className="me-2 " src={foodDelivery} alt="" />
+                    <span className="food">Food delivery</span>
+                </div >
+                <div className="row my-3 ">
+                    <div className="col-6 mt-3 ps-3  ">
+                        <p className="face">Нүүр</p>
+                        <p className="face">Хоолны цэс</p>
+                        <p className="face">Xүргэлтийн бүс</p>
+                    </div>
+                    <div className="col-6 mt-3 ">
+                        <p className="face">Холбоо барих</p>
+                        <p className="nuuts">(976) 77123456</p>
+                        <p className="nuuts">(976) 77123456</p>
+                    </div>
                 </div>
-                <div className="col-6">
-                    <h2>Холбоо барих</h2>
-                    <p>(976) 77123456</p>
-                    <p>(976) 77123456</p>
+                <div className="Logos text-center margin100">
+                    <img className="m-2" src={facebookicon} alt="facebook" />
+                    <img className="m-2" src={instagramIcon} alt="instagram" />
+                    <img className="m-2" src={twitterIcon} alt="twitter" />
                 </div>
-
-            </div>
-            <div className="Logos">
-            <i class="fab fa-facebook"></i>
-            
-
-            </div>
-            <img src="" alt="" srcset="" />
-            <div>
-                <p>Нууцлалын бодлого</p>
-                <p>Үйлчилгээний нөхцөл</p>
-
-            </div>
-            <div className="text-end">
-                <p>2020 Mstars Foods LLC</p>
-            </div>
+                <div className="text-center"> <img src={lineIcon} alt="" srcset="" /></div>
+               
+                <div className="ps-1">
+                    <p className="nuuts">Нууцлалын бодлого</p>
+                    <p className="nuuts">Үйлчилгээний нөхцөл</p>
+                </div>
+                <div className="text-end pb-5">
+                    <p className="last">&copy; 2020 Mstars Foods LLC</p>
+                </div>
             </div>
         </footer>
     )
