40556214e9a86ea3ce1a1c6236da9dadd6dd6bad
MStars Academy <mstars.developer@gmail.com>
2021-12-29 16:26:09 +0800

    fixing conflicts

--- project/src/App.js
+++ project/src/App.js
@@ -1,27 +1,13 @@
-<<<<<<< HEAD
-import './css/button.css'
-import Button from './components/Button';
-
-function App() {
-  return (
-     <div>
-       <Button class="nevtrehMobileView" type="НЭВТРЭХ"/>
-      <Button class="loginMobileView" type="ҮРГЭЛЖЛҮҮЛЭХ"/>
-      <Button class="loginMobileView" type="ХАДГАЛАХ"/>
-      <Button class="signupMobileView" type="БҮРТГҮҮЛЭХ"/>
-      <Button class="orderMobileView" type="САГСЛАХ"/>
-     </div>
-=======
-import './App.css';
-import Footer from './components/Footer';
+import "./App.css";
+import Footer from "./components/Footer";
 import Slider from "./components/Slider";
+
 function App() {
   return (
     <div>
       <Slider />
       <Footer />
     </div>
->>>>>>> 67ef9017f69f614b7c9224e3d200d6fd0864fad0
   );
 }
 export default App;
--- project/src/index.js
+++ project/src/index.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/button.css'
-import App from './App';
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/slider.css";
->>>>>>> 67ef9017f69f614b7c9224e3d200d6fd0864fad0
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,7 +12,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-<<<<<<< HEAD
-=======
 reportWebVitals();
->>>>>>> 67ef9017f69f614b7c9224e3d200d6fd0864fad0
