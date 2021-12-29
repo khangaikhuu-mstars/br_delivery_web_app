153b76b66ac2568db41798cbf0e7363bf95d5dd7
MStars Academy <mstars.developer@gmail.com>
2021-12-29 16:57:34 +0800

    css changed

--- project/src/components/Button.js
+++ project/src/components/Button.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-
+import "../css/button.css";
 const Buttons = (props) => {
   return <Button className={props.class}>{props.type}</Button>;
 };
