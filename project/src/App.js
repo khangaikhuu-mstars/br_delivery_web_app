4c6f5fd915f88a5e7fc04bd7efb0d140a200ecca
Mstars_Lab1_12 <mstars_lab1_12@Mstars-Lab1-12s-iMac.local>
2021-12-30 11:42:59 +0800

    #21 CategoryLabel check

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import CategoryLabel from './components/CategoryLabel';
 import Footer from './components/Footer';
 import Slider from "./components/Slider";
+import InfoPanel from './components/InfoPanel'
 function App() {
   return (
     <div>
-      <Slider />
-      <Footer />
+      {/* <Slider />
+      {/* <InfoPanel.js/> */
+      /* <Footer /> */ }
+      <CategoryLabel title='Хямдралтай'/>
+      <CategoryLabel title='Үндсэн хоол'/>
+      <CategoryLabel title='Салат ба зууш'/>
+      <CategoryLabel title='амттан'/>
+      
     </div>
   );
 }
