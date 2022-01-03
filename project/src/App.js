69a5c4e910d83d740251ae947014ad8171850dd8
Mstars_Lab1_12 <mstars_lab1_12@Mstars-Lab1-12s-iMac.local>
2022-01-03 15:46:06 +0800

    #31 standart

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './components/Footer';
 import Slider from "./components/Slider";
+import DeliveryZone from './components/DeliveryZone'
 function App() {
   return (
     <div>
       <Slider />
       <Footer />
+      <DeliveryZone/>
     </div>
   );
 }
