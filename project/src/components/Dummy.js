8374b84063023dd03715a02a7ecd777b06b54054
Mstars_Lab1_17 <mstarsacademy@Mstarss-iMac.local>
2022-01-03 14:30:38 +0800

    #36 adding buttons, styles and some routes

---
+++ project/src/components/Dummy.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, NavLink, Redirect } from "react-router-dom";
+
+const Dummy = () => {
+  return (
+    <div>
+      <h1>This is dummy component.</h1>
+    </div>
+  );
+};
+
+export default Dummy;
--- project/src/components/PageInsideMenu.js
+++ project/src/components/PageInsideMenu.js
@@ -1,10 +1,35 @@
 import React from "react";
-import Button from "./Button";
+import { Route, NavLink, Redirect } from "react-router-dom";
+import "../css/pageInsideMenu.css";
+import "../components/Dummy";
 
 const PageInsideMenu = () => {
   return (
-    <div className="main">
-      <h1>Hello</h1>
+    <div className="slidingButtons" id="slidingButtons">
+      <a href="" className="button button-active">
+        Үндсэн хоол
+      </a>
+      <a href="" className="button">
+        Салад ба зууш
+      </a>
+      <a href="" className="button">
+        Амттан
+      </a>
+      <a href="" className="button">
+        Хямдралтай
+      </a>
+      <NavLink className="button" to="/dummy">
+        Үндсэн хоол
+      </NavLink>
+      <NavLink className="button" to="/dummy">
+        Салад ба зууш
+      </NavLink>
+      <NavLink className="button" to="/dummy">
+        Амттан
+      </NavLink>
+      <NavLink className="button" to="/dummy">
+        Хямдралтай
+      </NavLink>
     </div>
   );
 };
