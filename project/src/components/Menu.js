4dce20828d668057945210d563ccce0333ad4e1a
Mstars_Lab1_17 <mstarsacademy@Mstarss-iMac.local>
2022-01-04 15:55:00 +0800

    #36 added cards on PageInsideMenu

--- project/src/components/Menu.js
+++ project/src/components/Menu.js
@@ -2,16 +2,20 @@ import React from "react";
 import PageInsideMenu from "./PageInsideMenu";
 import { Route, NavLink, Redirect, Switch } from "react-router-dom";
 import Dummy from "./Dummy";
+import MainDishMenu from "./MainDishMenu";
+import Salad from "./Salad";
+import Desserts from "./Desserts";
+import Sales from "./Sales";
 
 function Menu() {
   return (
     <div>
       <PageInsideMenu />
       <Switch>
-        <Route exact path="/menu" component={Dummy} />
-        <Route path="/menu/salad" component={Dummy} />
-        <Route path="/menu/snack" component={Dummy} />
-        <Route path="/menu/sale" component={Dummy} />
+        <Route exact path="/menu" component={MainDishMenu} />
+        <Route path="/menu/salad" component={Salad} />
+        <Route path="/menu/snack" component={Desserts} />
+        <Route path="/menu/sale" component={Sales} />
       </Switch>
     </div>
   );
