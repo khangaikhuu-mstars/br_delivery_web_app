c1b88091eb216749bd90576e870d5e021c2e67fe
Anar Ariuntulga <a.anar0918zero@gmail.com>
2022-01-05 12:22:40 +0800

    036accbb1f40650c1889ee5b2032b2ee5742ed90 #7

---
+++ project/src/components/Home 2.js
@@ -0,0 +1,35 @@
+import React from "react";
+// import "./App.css";
+import HeaderMenu from './navbar'
+import InfoPanel from "./InfoPanel";
+import Footer from "./Footer";
+import Slider from "./Slider";
+import "../../src/App.css" 
+import TextInput from "./TextInput";
+
+function Home() {
+    return (
+        <div>
+            <Slider />
+            <InfoPanel />
+
+            <div className="d-flex flex-column">
+                <Slider />
+
+                <div className="inputContainer">
+                    <TextInput name={'email'} type={'email'} />
+                    <TextInput name={'password'} type={'password'} />
+                    {/* <TextInput name={'password'} type={'password'}/>
+                        <TextInput name={'name'} /> */}
+                    <TextInput name={'number'} type={'number'} />
+                    <TextInput name={'textField'} type={'textField'} />
+                </div>
+
+            </div>
+
+        </div>
+    );
+
+}
+
+export default Home;
