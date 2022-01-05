1cd8d81a45a28b59b55a0376c8e83ff470ffa9b0
Anar Ariuntulga <a.anar0918zero@gmail.com>
2022-01-05 14:04:28 +0800

    #7 -

---
+++ project/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TextInput from "./TextInput";
+
+const Login = () => {
+    return (
+        <div>
+            <TextInput type={'password'}/>
+        </div>
+    )
+}
+
+export default Login;
