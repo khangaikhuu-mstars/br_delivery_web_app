99908b23367a66d24e23ff074928f5b0a22a0ebe
Mstars_Lab1_12 <mstars_lab1_12@Mstars-Lab1-12s-iMac.local>
2022-01-06 18:02:48 +0800

    #57 uuchlaarai gants 1 ym  save hiigeegui bn oo

--- project/src/components/Button.js
+++ project/src/components/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import "../css/button.css";
 const Buttons = (props) => {
-  return <Button type={props.type} className={props.class}>{props.value}</Button>;
+  return <Button  type={props.type} className={props.class}>{props.value}</Button>;
 };
 
 
