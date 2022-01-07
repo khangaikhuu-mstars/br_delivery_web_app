696f9a483bde585c03b76f1403987aa255a54e1d
Anar Ariuntulga <a.anar0918zero@gmail.com>
2022-01-08 01:42:46 +0800

    #79 [ADD] AddFood.js

---
+++ project/src/components/AddFood.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Buttons from './Button'
+
+
+const AddFood = () => {
+
+    return (
+        <div className="add-food">
+            <div>
+                <h5 className='modalHeader'>Хэмжээ</h5>
+                <div className='modalSizeButtons'>
+                    <button className='sizeButton  activeButton '>1 хүн</button>
+                    <button className='sizeButton '>2 хүн</button>
+                </div>
+            </div>
+
+            <h5 className='modalHeader'>Тоо</h5>
+            <div className='modalCounterButtons'>
+                <button className='counterButton '>-</button>
+                <p className='counterNumber '>10</p>
+                <button className='counterButton '>+</button>
+            </div>
+            
+            {/* <Buttons class={'orderTabletView'} type={'Захиалах'}/> */}
+            <button className='orderButton'>Захиалах</button>
+        </div>
+    );
+}
+
+export default AddFood;
