eadc1aafc184bb14d31cdf4f6b6deb59ae2c5490
Amarsanaa Munkhbat <amarsanaa0503@gmail.com>
2022-01-10 02:49:48 +0800

    #38 [ADD] Cart items component

--- project/src/components/CartSidebar.js
+++ project/src/components/CartSidebar.js
@@ -2,36 +2,39 @@ import React, { useState } from "react";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import Button from "react-bootstrap/Button";
 import "../css/offcanvas.css";
+import CartItems from "./CartItems";
 
 function CartSidebar() {
-  const [show, setShow] = useState(false);
+   const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+   const handleClose = () => setShow(false);
+   const handleShow = () => setShow(true);
 
-  return (
-    <>
-      {/* <Button variant="primary" onClick={handleShow}>
+   return (
+      <>
+         {/* <Button variant="primary" onClick={handleShow}>
         Launch
       </Button> */}
 
-      <img
-        src="/icons/busketicon.svg"
-        onClick={handleShow}
-        className="cart-icon"
-      />
+         <img
+            src="/icons/busketicon.svg"
+            onClick={handleShow}
+            className="cart-icon"
+         />
 
-      <Offcanvas show={show} onHide={handleClose} placement="end">
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title className="offcanvas-title">
-            Таны сагс
-          </Offcanvas.Title>
-        </Offcanvas.Header>
-        <span className="title-underline"></span>
-        <Offcanvas.Body>CART COMPONENT IS HERE!</Offcanvas.Body>
-      </Offcanvas>
-    </>
-  );
+         <Offcanvas show={show} onHide={handleClose} placement="end">
+            <Offcanvas.Header closeButton>
+               <Offcanvas.Title className="offcanvas-title">
+                  Таны сагс
+               </Offcanvas.Title>
+            </Offcanvas.Header>
+            <span className="title-underline"></span>
+            <Offcanvas.Body>
+               <CartItems />
+            </Offcanvas.Body>
+         </Offcanvas>
+      </>
+   );
 }
 
 export default CartSidebar;
