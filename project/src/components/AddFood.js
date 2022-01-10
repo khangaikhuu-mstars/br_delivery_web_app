d2644ae5fdbf6efff30d8a22e404d872ec1bac64
Mstars_lab1_16 <mstarsacademy@MStarss-Anar.local>
2022-01-10 16:11:53 +0800

    #79 portion problem solved

--- project/src/components/AddFood.js
+++ project/src/components/AddFood.js
@@ -3,40 +3,62 @@ import Buttons from "./Button";
 import "../css/addfood.css";
 
 const AddFood = () => {
-    
   const [count, setCount] = useState(1);
-  
-  const handleCounter = (num) => {
-      console.log(num)
-    setCount(count =>
-        count > 0 ? count += num : count = 1
-    );
+  const [portion, setPortionSize] = useState(1);
+  const [active, setActiveButton] = useState(true);
+
+  const handlePortion = (size) => {
+    setActiveButton(() => !active);
+    setPortionSize(() => size);
   };
 
-  const sendFoodCount = (num) => {
-      console.log('Ordered food', num);
-  }
+  const handleCounter = (num) => {
+    setCount((count) => (count > 0 ? (count += num) : (count = 1)));
+  };
+
+  const sendFoodCount = (count, size) => {
+    console.log("Ordered food:", count, "Portion:", size);
+  };
 
   return (
     <div className="add-food">
       <div>
         <h5 className="modalHeader">Хэмжээ</h5>
         <div className="modalSizeButtons">
-          <button className="sizeButton" onClick={()=> setCount(1)}>1 хүн</button>
-          <button className="sizeButton" onClick={()=> setCount(2)}>2 хүн</button>
+          <button
+            className={`sizeButton ${active ? "activeButton" : ""}`}
+            onClick={() => handlePortion(1)}
+          >
+            1 хүн
+          </button>
+          <button
+            className={`sizeButton ${!active ? "activeButton" : ""}`}
+            onClick={() => handlePortion(2)}
+          >
+            2 хүн
+          </button>
         </div>
       </div>
 
       <h5 className="modalHeader">Тоо</h5>
       <div className="modalCounterButtons">
-        <button className="counterButton" onClick={()=> handleCounter(-1)}>-</button>
+        <button className="counterButton" onClick={() => handleCounter(-1)}>
+          -
+        </button>
         <p className="counterNumber "> {count} </p>
-        <button className="counterButton" onClick={()=> handleCounter(1)}>+</button>
+        <button className="counterButton" onClick={() => handleCounter(1)}>
+          +
+        </button>
       </div>
 
       {/* <Buttons class={"orderTabletView"} type={"Захиалах"} /> */}
       {/* <Buttons class={'orderTabletView orderButton'} type={'Захиалах'}/> */}
-      <button className='orderButton' onClick={()=> sendFoodCount(count)}>Захиалах</button>
+      <button
+        className="orderButton"
+        onClick={() => sendFoodCount(count, portion)}
+      >
+        Захиалах
+      </button>
     </div>
   );
 };
