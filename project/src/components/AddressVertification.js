0a2631e082e7db0b34caf7c6153a01aaf1565028
tuvshinbat t <tuvshinbat@gmail.com>
2022-01-10 21:28:17 +0800

    #75 adress  hiigdej baigaa

--- project/src/components/AddressVertification.js
+++ project/src/components/AddressVertification.js
@@ -4,51 +4,53 @@ import "../css/adress.css";
 
 
 const AddressVertification = () => {
+
     return (
         <div className="container">
             <div className="row">
-                <div className="col-lg-6">
+                <div className="col-12  col-lg-6">
                     <div >
                         <form action="" className="forms">
-                            <p>Хаягын мэдээлэл</p>
+                            <p className="p-2 m-2 text-shargal">Хаягын мэдээлэл</p>
 
-                            <select name="" id="" className="col-12">
-                                
+                            <select name="" id="" className="col-12 m-2 p-2 inputStyle selectbox">
+
                                 <option value="">Хан-Уул Дүүрэг </option>
                                 <option value="">Баянзүрэх Дүүрэг</option>
                                 <option value="">СүхБаатар Дүүрэг</option>
                                 <option value="">Сонгинохайрхан Дүүрэг</option>
                             </select>
-                            <select name="" placeholder="Хороо сонгоно уу" className="col-12" id="">
-                                <option value="">Хороо сонгоно уу</option>
+                            <select name="" placeholder="Хороо сонгоно уу" className="inputStyle col-12 p-2 m-2" id="">
+                                <option value=""> Хороо сонгоно уу</option>
                                 <option value="">1-р Хороо</option>
                                 <option value="">2-р Хороо</option>
                             </select>
-                            <select name="" className="col-12"  id="">
+                            <select name="" className="col-12 p-2 m-2 inputStyle" id="">
                                 <option value="">Байр, гудамж сонгоно уу.</option>
                                 <option value="">1-р байр</option>
                                 <option value="">89-р байр</option>
                             </select>
-                            <p > Хаягын дэлэгрэнгүй</p>
-                            <textarea className="col-12 inputStyle" name="" id="" cols="20" rows="5" placeholder="Орц, даврах, орцны код... "></textarea>
-                            <p>Утасны дугаар</p>
-                            <input type="text" className="col-12 inputStyle" placeholder="Утасны дугаар оруулна уу."/>
-                            <p>Төлбөр төлөх</p>
-                            <p className="col-12 d-flex justify-content-between"><span> <input type="checkbox" />Бэлнээр</span>
-                            <span><input type="checkbox"  /> Картаар</span>
-                            
+                            <p className="p-2 m-2 text-shargal" > Хаягын дэлэгрэнгүй</p>
+                            <textarea className="col-12 p-2 m-2 inputStyle" name="" id="" cols="20" rows="5" placeholder="Орц, даврах, орцны код... "></textarea>
+                            <p className="p-2 m-2text-shargal">Утасны дугаар</p>
+                            <input type="text" className="col-12 p-2 m-2 inputStyle" placeholder="Утасны дугаар оруулна уу." />
+                            <p className="p-2 m-2 text-shargal">Төлбөр төлөх</p>
+                            <p className="col-12 p-2 m-2 d-flex justify-content-start">
+                                <span className="col-6"> <input className="checkbox" type="checkbox" />Бэлнээр</span>
+                                <span className="col-6"><input className="checkbox" type="checkbox" /> Картаар</span>
+
                             </p>
                             <img src={navlogo} alt="" className="uuu" />
-                            <img src="/icons/navlogo.svg" className="uuu"/>
-                            <input type="button" value="Захиалах" className="col-12 shargal11"/>
-                           
-                            
+
+                            <input type="button" value="Захиалах" className="col-12 shargal11" />
+
+
                         </form>
-                       
+
                     </div>
 
                 </div>
-                <div className="col-lg-6">
+                <div className="col-12 col-lg-6">
 
 
                 </div>
