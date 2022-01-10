f4e8b3e8ef10f785b39ed99c45354049b2068bef
Mstars_lab1_16 <mstarsacademy@MStarss-Anar.local>
2022-01-10 16:46:20 +0800

    #79 portion toggler issue solved

--- project/src/components/AddFood.js
+++ project/src/components/AddFood.js
@@ -6,12 +6,10 @@ const AddFood = () => {
   const [count, setCount] = useState(1);
   const [portion, setPortionSize] = useState(1);
   const [active, setActive] = useState(true);
-  const [activeButton, setActiveButton] = useState('add1');
+  const [activeButton, setActiveButton] = useState("add1");
 
   const handlePortion = (size) => {
-    setActive(() => 
-        !active
-    );
+    setActive(() => !active);
     setPortionSize(() => size);
   };
 
@@ -29,24 +27,26 @@ const AddFood = () => {
         <h5 className="modalHeader">Хэмжээ</h5>
         <div className="modalSizeButtons">
           <button
-            id='add1'
-            className={`sizeButton ${activeButton === 'add1' && active ? "activeButton" : ""}`}
+            id="add1"
+            className={`sizeButton ${
+              activeButton === "add1" ? "activeButton" : ""
+            }`}
             onClick={(event) => {
-              console.log(event.target.id);
-              setActiveButton("add1")
+              setActiveButton("add1");
               handlePortion(1);
             }}
           >
             1 хүн
           </button>
           <button
-            id='add2'
-            className={`sizeButton ${activeButton === 'add2' && !active ? "activeButton" : ""}`}
+            id="add2"
+            className={`sizeButton ${
+              activeButton === "add2" ? "activeButton" : ""
+            }`}
             onClick={(event) => {
-                console.log(event.target.id);
-                setActiveButton("add2")
-                handlePortion(2);
-              }}
+              setActiveButton("add2");
+              handlePortion(2);
+            }}
           >
             2 хүн
           </button>
