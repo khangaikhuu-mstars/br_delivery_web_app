1adbcf884152f4c5937e7dbab5fc42cb02c1108c
Mstars_Lab1_12 <mstars_lab1_12@Mstars-Lab1-12s-iMac.local>
2022-01-11 15:43:06 +0800

    #57 forget password finish

--- project/src/components/ForgetPassword.js
+++ project/src/components/ForgetPassword.js
@@ -13,7 +13,11 @@ const ForgetPassword = () => {
     const clickHandler = () => {
         setCount(count + 1)
     }
-
+    const [email, setEmail] = useState('')
+    
+    const handleChange= (event) =>{
+       setEmail(event.target.value)
+      }
     return (
         <div className="container my-3">
             <form className="row flex-column mehanic-margin">
@@ -21,7 +25,7 @@ const ForgetPassword = () => {
                     <h5 className="col-12 ps-3 text-dark border-left">НУУЦ ҮГ СЭРГЭЭХ</h5>
                 </NavLink >
                 <div className={show ? "hideclass" : "showclass forgetpassword1 col-10 col-sm-8 col-md-7 col-lg-6 py-2 my-2   "}>
-                    <p className="">Таны <span className="orange">"mstars@exampple.com" </span>  хаяг руу нууц үг сэргээх код илгээх болно. </p>
+                    <p className="">Таны <span className="orange">"{email}" </span>  хаяг руу нууц үг сэргээх код илгээх болно. </p>
                 </div>
                 <div className={show ? "hideclass " : "showclass py-2 my-2 row     "}>
                     <label className="col-12 p-0 text-dark" for="passwordInput">Нууц үг сэргээх код</label>
@@ -29,7 +33,7 @@ const ForgetPassword = () => {
                 </div>
                 <div className={show ? " showclass py-2 my-2 row " : "hideclass "}>
                     <label className="col-12 p-0 text-dark" for="passwordInput">И-мэйл</label>
-                    <input type="text" id="passwordInput" className="col-sm-10 col-md-8 col-lg-7 ps-2 py-2 forgetpassword1   my-3 " placeholder="И-мэйл хаягаа оруулна уу" />
+                    <input type="text" id="passwordInput" onChange={handleChange} className="col-sm-10 col-md-8 col-lg-7 ps-2 py-2 forgetpassword1   my-3 " placeholder="И-мэйл хаягаа оруулна уу" />
                 </div>
 
                 <div className={(count === 1) ? 'hideclass' : "showclass py-2 my-2 row"}>
